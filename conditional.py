ae914eef10c6a111e6681855c6a263b2f145b4ce
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-10 20:44:00 +0545

    Added the conditional statement example

---
+++ conditional.py
@@ -0,0 +1,6 @@
+# Write a program to check whether the number is even or odd.
+num=int(input("Enter the number: "))
+if num%2==0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} is odd number")
