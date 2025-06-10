f264594602d6ee03d9a97d9db088610079f21d63
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-10 20:36:17 +0545

    Added the operators in python

---
+++ operators.py
@@ -0,0 +1,15 @@
+# Write a python program to demonstrate the arithmetic operators in python.
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+add=a+b
+sub=a-b
+multiply=a*b
+division=a/b
+modulos=a%b
+exponent=a**b
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiply: {multiply}")
+print(f"Division: {division}")
+print(f"Modulus: {modulos}")
+print(f"Exponent: {exponent}")
