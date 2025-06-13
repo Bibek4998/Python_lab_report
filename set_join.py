248b81f4e7fdf60f43b888bf6fc0d26e9ca265d4
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-13 14:07:47 +0545

    Joining the two sets

---
+++ set_join.py
@@ -0,0 +1,7 @@
+# Write a python program to join the two sets.
+set1={"Dhangadhi","Mahendranagar","Pokhara","Bharatpur"}
+set2={"Kailali","Kanchanpur","Kaski","Chitwan"}
+print("-----Joining the set-----")
+set=set1.union(set2)
+for sets in set:
+    print(sets)
