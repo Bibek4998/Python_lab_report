d4288e8f7d9bc7a39b2b425b0158d70b48d40474
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-13 13:51:41 +0545

    Joining tuples

---
+++ tuple_join.py
@@ -0,0 +1,9 @@
+# Write a program to join tuple.
+tuple1=[1,2,3,4,5,6]
+tuple2=['A','B','C','D','E']
+joining=tuple1+tuple2
+print("---Joining tuple using (+)---")
+print(joining)
+print("---Joining tuples using (*)---")
+tuples=tuple1*2
+print(tuples)
