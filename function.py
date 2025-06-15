2308b6f6c53b7fe53ef2a22f4e185c8fd20ddcf6
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 20:52:30 +0545

    Function example

---
+++ function.py
@@ -0,0 +1,9 @@
+# Write a python program to make the function pass the two parameters and return the sum of them.
+def find_sum(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = find_sum(num1, num2)
+print("The sum is:", result)
