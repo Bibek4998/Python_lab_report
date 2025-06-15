efc70845252869875042c1d8818a8d0f85e92c39
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 21:07:16 +0545

    Class example in python

---
+++ class.py
@@ -0,0 +1,9 @@
+# Create a class named person and use the __init__() function to assign values for name and age:
+class person:
+    def __init__(self,name ,age):
+        self.name=name
+        self.age=age
+        
+a=person("Bibek Raj Joshi",19)
+print(a.name)
+print(a.age)
