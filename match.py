373aa03b953b97564633b3d05593206305e5b0ce
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 20:41:34 +0545

    Match case statement

---
+++ match.py
@@ -0,0 +1,19 @@
+# Write a python program to show the use of match case statement.
+day=int(input("Enter the day number: "))
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+  case default:
+    print("Invalid")
