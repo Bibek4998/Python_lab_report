847459a9db5b20e5865703c9631e04e7edfd3c42
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 21:00:35 +0545

    lamda function

---
+++ lamda.py
@@ -0,0 +1,4 @@
+# Write a python program to demonstrate the example of lamda function.
+sum=lambda: int(input("Enter the first number: "))+int(input("Enter the second number: "))
+add=sum()
+print(f"The sum is: {add}")
