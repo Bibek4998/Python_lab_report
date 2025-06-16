f479b9248a40a61ab3ded75126e28d5e9a2e9ad8
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-16 21:38:41 +0545

    Added the series file

---
+++ series.py
@@ -0,0 +1,6 @@
+# Write a python program to make the series and display the result.
+import pandas as pd
+data=[10,12,11,13,15,1,89]
+index=["a","b","c","d","e","f","g"]
+ser=pd.Series(data,index=index)
+print(ser)
